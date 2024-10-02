740acff0a27d303e7dbc71e8ca250bec0c7e8adb
coryzed <coryz@outlook.com>
2024-10-02 13:53:10 -0600

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'jumbo-bg': "url('src/assets/nwmain.png')",
+			},
+			colors: {
+			  // primary: defaultTheme.colors.green
+				'nwm-blue': '#25B5FE',
+				'nwm-lblue': '#DDF2FC',
+				'nwm-pdfblue': '#1481ab',
+				'nwm-green': '#4AC3BA',
+			},
+		  }
+	},
+	plugins: [],
+}
